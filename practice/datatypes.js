d402f49690531ee98b4b05cb860abc5c85222843
jaiswaldev <137531632+jaiswaldev@users.noreply.github.com>
2024-06-02 10:46:26 +0000

    datatypes and operations

---
+++ practice/datatypes.js
@@ -0,0 +1,58 @@
+"use strict";  //treat all js code as newer version.
+
+// alert(2+4) // it shows error in nodejs. but runs in browser perfectly, bcz in nodejs its syntx in different which will study later.
+
+// int => range 2^53
+// bigint
+//string => "" or ''
+// boolean => true or false
+// null
+// undefined
+// symbol
+
+// object
+var accountemail= "jaiswaldev479@gmail.com"
+
+typeof(accountemail);  // use to know the type of variable.
+
+// console.log(typeof(null)) // object.
+// console.log(typeof(undefined))  // undefined.
+
+
+// let score= "44tyy"
+// console.log(typeof(score)) 
+
+// let value= Number(score)
+// console.log(typeof(value)) 
+// console.log(value)
+
+
+// "44"= 44
+// "44ass" = NaN
+// null= 0
+// undefined= NaN
+// true/false= 1/0
+// "" = NaN
+
+
+let score= "jjj"
+console.log(typeof(score)) 
+
+let value= Boolean(score)
+console.log(typeof(value)) 
+console.log(value)
+
+
+//1= true
+//0=false
+//""=false
+//"yhyb"=true
+
+
+// we can also convert in string by using String keyword.
+
+
+
+
+
+
---
+++ practice/variables.js
@@ -0,0 +1,15 @@
+console.log("Dev jaiswal")
+
+const accountid = 2214136
+var accountemail= "jaiswaldev479@gmail.com"
+let accountpss= "123456"
+city="neemkathana"   //this can be possible in js. but preferably not to use.
+let= accountstate;   // if we try to print this variable. it will show undefined.
+// accountid=2234243  not allowed with constants.
+
+//  var and let can be changed. but prefer not to use var bcz of 
+//   its issue in block scope and functional scope.
+
+console.log(accountid);
+console.table([accountid,accountemail,accountpss,city])
+
