54b29e9997c7b5cae93d35ab247f28f3b69cf43f
jaiswaldev <137531632+jaiswaldev@users.noreply.github.com>
2024-06-15 11:53:00 +0000

    controlflow

---
+++ control_flow/ifelse.js
@@ -0,0 +1,10 @@
+// if(true){  // if inside bracket statement is false then it does'nt execute.
+
+// }
+//  else{    // if statement inside if is true then if ke andr ka code execute hoga but agr it is false then else wala execute hoga.
+
+//  }
+
+//comparision operators.
+// < ,> ,<=,>=,==, !=,===,!==
+
---
+++ control_flow/switch.js
@@ -0,0 +1,52 @@
+// switch (key) {      //syntax of switch.
+//     case value:    
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month =3;
+// switch (month) { 
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("febuary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     default:
+//         console.log("nothing");
+//        break;
+// }  // agr hum khepr break nhi lgate hai toh uske baad ke saare cases execute honge except default.
+
+// const usermail=""
+// if(usermail){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+// falsy values--> false,0,-0,"",null,undefined,NaN,Bigint 0n.(that is assume to be always false)
+//truthy values--> "0",'false'," "(space inside string),[],{},function(){}.(that is assume to be always true)
+
+//false ==0  true.
+//false==''  true.
+//0==''    true.
+
+//Nullish coalescing operator (??).null undefined
+
+let val
+//val=20??10
+//val=null??15
+// val=undefined ?? null ??12
+// console.log(val);
+
+
+// Ternary operator.
+condition ? true: false
+
+ 
